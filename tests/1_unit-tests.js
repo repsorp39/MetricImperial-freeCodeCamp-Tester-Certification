31691510904f7d35e3ca11916b5b8c182e31a3b0
Prosper <hazardprosper227@gmail.com>
2025-03-14 13:20:00 +0100

    changing keywords it to test to fit fcc rules

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -10,33 +10,33 @@ suite('Unit Tests', function(){
         assert.equal(convertHandler.getNum(),15);
     })
     
-    it("should correctly read a decimal number input",()=>{
+    test("should correctly read a decimal number input",()=>{
         let convertHandler = new ConvertHandler("15.888mi");
         assert.equal(convertHandler.getNum(),15.888);
     })
 
-    it("should correctly read a fractional input",()=>{
+    test("should correctly read a fractional input",()=>{
         let convertHandler = new ConvertHandler("15/12mi");
         assert.equal(convertHandler.getNum(),15/12);
     })
 
 
-    it("should correctly read a fractional input with a decimal",()=>{
+    test("should correctly read a fractional input with a decimal",()=>{
         let convertHandler = new ConvertHandler("15/12.5mi");
         assert.equal(convertHandler.getNum(),15/12.5);
     })
 
-    it("should correctly return an error on a double-fraction",()=>{
+    test("should correctly return an error on a double-fraction",()=>{
         let convertHandler = new ConvertHandler("15/12/12");
        expect(convertHandler.getNum()).not.to.be.a("number");
     })
 
-    it("should correctly default to a numerical input of 1 when no numerical input is provided",()=>{
+    test("should correctly default to a numerical input of 1 when no numerical input is provided",()=>{
         let cvt = new ConvertHandler("mi");
        assert.equal(cvt.getNum(),1);
     })
 
-    it("should correctly read each valid input unit",()=>{
+    test("should correctly read each valid input unit",()=>{
         let cvt1 = new ConvertHandler("10mi");
         let cvt2 = new ConvertHandler("10L");
         let cvt3 = new ConvertHandler("10gal");
@@ -45,12 +45,12 @@ suite('Unit Tests', function(){
         assert.equal(cvt3.getUnit(),"gal");
     })
     
-    it("should correctly return an error for an invalid input unit",()=>{
+    test("should correctly return an error for an invalid input unit",()=>{
         let cvt = new ConvertHandler("10");
         assert.equal(cvt.getUnit(),"");
     })
 
-    it("should return the correct return unit for each valid input unit",()=>{
+    test("should return the correct return unit for each valid input unit",()=>{
         let cvt1 = new ConvertHandler("10mi");
         let cvt2 = new ConvertHandler("10l");
         let cvt3 = new ConvertHandler("10gal");
@@ -60,38 +60,38 @@ suite('Unit Tests', function(){
         assert.equal(cvt3.getReturnUnit(),"L");
     })
 
-    it("should correctly return the spelled-out string unit for each valid input unit",()=>{
+    test("should correctly return the spelled-out string unit for each valid input unit",()=>{
         const cvt1 = new ConvertHandler("10mi");
         const exp1 = "10 miles converts to 16.09340 kilometers";
         assert.equal(cvt1.getString(),exp1);
     })
  
-    it("should correctly convert gal to L",()=>{
+    test("should correctly convert gal to L",()=>{
         let cvt = new ConvertHandler("10gal");
         assert.equal(cvt.convert(),37.8541);
     })
 
-    it("should correctly convert L to gal",()=>{
+    test("should correctly convert L to gal",()=>{
         let cvt = new ConvertHandler("37.8541L");
         assert.equal(cvt.convert(),10);
     })
 
-    it("should correctly convert mi to km",()=>{
+    test("should correctly convert mi to km",()=>{
         let cvt = new ConvertHandler("10mi");
         assert.equal(cvt.convert(),16.0934);
     })
 
-    it("should correctly convert km to mi",()=>{
+    test("should correctly convert km to mi",()=>{
         let cvt = new ConvertHandler("16.0934km");
         assert.equal(cvt.convert(),10);
     })
 
-    it("should correctly convert lbs to kg",()=>{
+    test("should correctly convert lbs to kg",()=>{
         let cvt = new ConvertHandler("10lbs");
         assert.equal(cvt.convert(),4.53592);
     })
 
-    it("should correctly convert kg to lbs",()=>{
+    test("should correctly convert kg to lbs",()=>{
         let cvt = new ConvertHandler("4.53592kg");
         assert.equal(cvt.convert(),10);
     })
